f638468d9a02b95a5aa06dbb4642d79274710c53
zsjon <150127747+zsjon@users.noreply.github.com>
2024-07-03 18:03:40 +0900

    [Silver II] Title: 로또, Time: 264 ms, Memory: 18400 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,42 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     static int k;
-    static int [] s;
-    static boolean [] chk;
-    public static void main(String[] args) throws  Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(true){
-            String testCase=br.readLine();
-            if(testCase.equals("0")) break;
-            String [] input=testCase.split(" ");
-            k=Integer.parseInt(input[0]);
-            s=new int[k];
-            chk=new boolean[k];
-            for(int i=0;i<k;i++){
-                s[i]=Integer.parseInt(input[i+1]);
-            } //초기 값 세팅
+    static int[] s;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
 
-            dfs(0,0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while (true) {
+            String st = br.readLine();
+            String []input = st.split(" ");
+            k = Integer.parseInt(input[0]);
+            if(k == 0) break;
+            s = new int[k];
+            visit = new boolean[k];
+            for (int i = 0; i < k; i++) {
+                s[i] = Integer.parseInt(input[i+1]);
+            }
+            backTracking(0, 0);
             System.out.println();
         }
-    }
+//        System.out.println(sb);
 
-    public static void dfs(int depth,int start){
-        if(depth==6){
-            for(int i=0;i<k;i++){
-                if(chk[i]){
+
+    }
+    static void backTracking(int start, int depth){
+        if(depth == 6){
+            for(int i = 0; i < k; i++){
+                if(visit[i]){
                     System.out.print(s[i]+" ");
                 }
             }
             System.out.println();
         }
-        for(int i=start;i<k;i++){
-            chk[i]=true;
-            dfs(depth+1,i+1);
-            chk[i]=false;
+        for(int i = start; i < k; i++){
+            visit[i] = true;
+            backTracking(i+1, depth+1);
+            visit[i] = false;
         }
     }
 }
