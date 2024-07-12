c334700e28255c0e93c721ad2a64ff34145751f7
zsjon <150127747+zsjon@users.noreply.github.com>
2024-07-12 12:55:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 312 ms, Memory: 23228 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = 0; j < arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int j : arr) {
+            System.out.print(j + "\n");
+        }
+
+    }
+
+}
