e88bd8a81c6f582d5745a04cc4a5821ac75fcd8e
Minseong Cho <150127747+zsjon@users.noreply.github.com>
2025-11-14 21:41:31 +0900

    [level 1] Title: 2016년, Time: 4.56 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+    const day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    let date = new Date(2016, a-1, b);
+    let answer = day[date.getDay()];
+    
+    return answer;
+}
