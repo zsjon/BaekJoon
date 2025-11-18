977b07975f104e9f780849087e6bb4498f901031
Minseong Cho <150127747+zsjon@users.noreply.github.com>
2025-11-18 16:42:21 +0900

    [level 1] Title: 예산, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,19 +1,5 @@
 function solution(d, budget) {
-    for(let i = 0; i < d.length; i++){
-        for(let j = 0; j < d.length - 1; j++){
-            if(d[j] > d[j+1]){
-                let temp = d[j];
-                d[j] = d[j+1];
-                d[j+1] = temp;
-            }
-        }
-    }
-    var cnt = 0;
-    let sum = 0;
-    for(i in d){
-        sum += d[i];
-        if(sum > budget) break;
-        cnt++;
-    }
-    return cnt;
+    return d.sort((a, b) => a - b).reduce((count, price) => {
+        return count + ((budget -= price) >= 0);
+    }, 0);
 }
