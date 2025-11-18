98614919df60c6300f2822f2b6504c42b3bb20d0
Minseong Cho <150127747+zsjon@users.noreply.github.com>
2025-11-18 19:02:30 +0900

    [level 1] Title: 실패율, Time: 5109.54 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=javascript) 
 
 ### 성능 요약
 
-메모리: 44.2 MB, 시간: 1315.81 ms
+메모리: 67.1 MB, 시간: 5109.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 18일 18:56:15
+2025년 11월 18일 19:02:27
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,12 +1,10 @@
 function solution(N, stages) {
     var answer = [];
-    let people = stages.length;
-    for(let i = 1; i <= N; i++){ //스테이지별 미클리어 인원 구하기
-        let fail = stages.filter((el) => el === i); //스테이지별 미클리어 인원 배열
-        let per = fail.length / people; //스테이지별 미클리어 비율
-        answer.push({stage: i, failed: per}); //객체로 배열에 저장
-        people -= fail.length; //전체 인원에서 미클리어 인원 차감
+    for(let i = 1; i <= N; i++){
+        let reach = stages.filter(el=>el>=i).length;
+        let curr = stages.filter(el=>el===i).length;
+        answer.push([i, curr/reach]);
     }
-    
-    return answer.sort((a, b) => b.failed - a.failed).map(el=>el.stage); //스테이지별 미클리어 비율만 내림차순 정렬
+    answer.sort((a, b) => b[1] - a[1]);
+    return answer.map(x => x[0]);
 }
