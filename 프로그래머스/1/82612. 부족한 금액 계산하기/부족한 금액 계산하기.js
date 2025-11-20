a9daef3a87867b7a64ef692d5d10b831662ca63a
Minseong Cho <150127747+zsjon@users.noreply.github.com>
2025-11-20 14:46:33 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,11 @@
+function solution(price, money, count) {
+    var answer = -1;
+    let sum = 0;
+    for(let i = 1; i <= count; i++){
+        let pricePerCnt = price * i;
+        sum += pricePerCnt;
+    }
+    if(money >= sum) answer = 0;
+    else answer = (sum - money);
+    return answer;
+}
